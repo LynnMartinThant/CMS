ac452bbdc8fe464f7bb7958fb98f49e0f0d1a5f7
Lynn_Martin_Thant <lynnthant.ko05@gmail.com>
2026-04-28 19:31:44 +0100

    Add findByID method to UserRepository
    Added a method to find a User by ID.

--- src/main/java/com/ABC/ABC_FComplaintWebapp/repositories/UserRepository.java
+++ src/main/java/com/ABC/ABC_FComplaintWebapp/repositories/UserRepository.java
@@ -8,16 +8,11 @@ import com.ABC.ABC_FComplaintWebapp.model.User;
 import java.util.Optional;
 import java.util.UUID;
 
-/**
- * SECURITY FIX_001: User Repository with Password Security
- * Weakness ID: Wk_001
- * Fix ID: Fix_001 – User Repository for BCrypt Password Management
- * D3FEND: D3-PH Password Hashing
- */
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     
     Optional<User> findByUsername(String username);
+    Optional <User> findByID(Long ID);
     
     Optional<User> findByEmail(String email);
     
